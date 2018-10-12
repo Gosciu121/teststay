094dbd22f43101684a48583a3a9961d192ccacf0
Gosciu121 <44103282+Gosciu121@users.noreply.github.com>
2018-10-13 00:22:56 +0200

    Add files via upload

---
+++ config/config.js
@@ -0,0 +1,12 @@
+module.exports.options = {
+    token: 'NTAwNDE2NDI2MjcyNjIwNTU5.DqKs3A.t13JpPcM4hlI_uQy0wQa3c5nmvE',
+    topicChannels: [
+      'general'
+    ],
+    roles: ["Admin"],
+    servers: [
+        { name: 'Stay-Alive', url: 'http://173.249.17.251:10120', alias: 'lista' },
+        { name: 'Server2', url: 'http://127.0.0.1:30121', alias: 'LosAngeles' }
+    ],
+    pollRate: 30,
+};
