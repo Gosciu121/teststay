9ff4ff32cf771be1d6ed600751baa2390fc1d4d9
Gosciu121 <44103282+Gosciu121@users.noreply.github.com>
2018-10-13 14:16:05 +0200

    Update main.js

--- main.js
+++ main.js
@@ -78,7 +78,7 @@ client.on('message', message => {
   // Check if the messager has the required role
   let foundRole = false;
   for (const role of options.roles) {
-    if (message.member.roles.find("🏠CYWIL", role)){
+    if (message.member.roles.find("name", role)){
       foundRole = true;
       break;
     }
