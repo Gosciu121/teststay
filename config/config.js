437cc32182a98ddcb10a879b9307b27b9115bc0f
Gosciu121 <44103282+Gosciu121@users.noreply.github.com>
2018-10-13 12:36:43 +0200

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,9 +1,9 @@
 module.exports.options = {
     token: 'NTAwNDE2NDI2MjcyNjIwNTU5.DqKs3A.t13JpPcM4hlI_uQy0wQa3c5nmvE',
     topicChannels: [
-      'lista'
+      'general'
     ],
-    roles: ["🏠CYWIL"],
+    roles: ["Admin"],
     servers: [
         { name: 'Stay-Alive', url: 'http://173.249.17.251:10120', alias: 'lista' }
         
