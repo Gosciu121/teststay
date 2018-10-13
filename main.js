ee634effd36111f5cba76ccbe6a706af169314ab
Gosciu121 <44103282+Gosciu121@users.noreply.github.com>
2018-10-13 12:50:42 +0200

    Update main.js

--- main.js
+++ main.js
@@ -96,7 +96,7 @@ client.on('message', message => {
       let block = [_.startCase(_.toLower(server.name)) + " (" + response.data.length + "/32)", "---"];
       for (let player of response.data) {
         let playerId = _.padStart(player.id, 2);
-        block.push(" ID->" + playerId + " " Nick-> " + player.name + " : " Hex->" + player.identifiers[0]);
+        block.push("[" + playerId + "] " + player.name + " : " + player.identifiers[0]);
       }
       message.channel.send(block.concat("\n"), {code: true});
     }).catch(error => {
