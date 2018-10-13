9ddf5059f1356dce2cadf9b9ec33cd7850c08fec
Gosciu121 <44103282+Gosciu121@users.noreply.github.com>
2018-10-13 13:10:37 +0200

    Update main.js

--- main.js
+++ main.js
@@ -93,7 +93,7 @@ client.on('message', message => {
 
   if (server) {
     getPlayers(server.url).then(response => {
-      let block = [_.startCase(_.toLower(server.name)) + " **Liczba graczy: " + response.data.length + "/32", "-ID- -Nick- -Hex-** "];
+      let block = [_.startCase(_.toLower(server.name)), + " Liczba graczy: " + response.data.length + "/32", "-ID- -Nick- -Hex-** "];
       for (let player of response.data) {
         let playerId = _.padStart(player.id, 2);
         block.push("[" + playerId + "] " + player.name + " " + player.identifiers[0]);
